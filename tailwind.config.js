70c284c08ca28cf2489ada6683fa6f74b1e88200
macbookpro <mahsinsaifullah@gmail.com>
2024-02-18 09:40:03 +0000

    chore: add custom color and font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      darkPurple: '#8F70C6',
+      brightPink: '#F597C1',
+    },
+    extend: {
+      fontFamily: {
+        rubik: ['"Rubik"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
