06633cacfe15946c00b2b534912aa1be10c65c54
Mahsin Saifullah <mahsinsaifullah@gmail.com>
2024-02-19 12:25:45 +0000

    feat: add HeroImage svg

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <main className="min-h-screen bg-cover bg-no-repeat bg-[url('gradientBG.svg')] py-10  flex flex-col">
       <Navbar navLinks={HOME_NAV_ITEMS} />
-      <div className="flex-1 flex flex-col justify-center">{children}</div>
+      {children}
       <Footer />
     </main>
   );
--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,11 +1,12 @@
+import { HeroImage } from 'assets';
 import { Layout } from 'components';
 
 export const Landing = () => {
   return (
     <Layout>
-      <div className="container mx-auto grid grid-cols-2 items-center">
-        <div>
-          <img src={'heroLanding.svg'} alt="" />
+      <div className='container mx-auto flex-1 grid grid-cols-2 justify-center items-center gap-2'>
+        <div className='flex justify-center'>
+          <HeroImage className='w-[750px] h-[750px]' />
         </div>
         <div>hello</div>
       </div>
