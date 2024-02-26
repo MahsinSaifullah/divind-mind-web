64c86157c6b7d85ce06911b23a294cfbd4312bf3
Mahsin Saifullah <mahsinsaifullah@gmail.com>
2024-02-26 17:18:47 +0000

    feat: add default nav link to landing page

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,10 +1,11 @@
 import { HeroImage } from 'assets';
 import { LandingLayout } from 'components';
 import { Details } from './Details';
+import { HOME_NAV_ITEMS } from 'constants';
 
 export const Landing = () => {
   return (
-    <LandingLayout>
+    <LandingLayout navLinks={HOME_NAV_ITEMS}>
       <div className="container mx-auto flex-1 grid grid-cols-2 justify-center items-center gap-2">
         <div className="flex justify-center">
           <HeroImage className="w-[750px] h-[750px]" />
