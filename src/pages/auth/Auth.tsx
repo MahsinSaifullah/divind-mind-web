74dfbc42883dbc7c1432bb28cd29e63f27edba5b
Mahsin Saifullah <mahsinsaifullah@gmail.com>
2024-02-26 16:21:05 +0000

    feat: add reusable logic to have the same screen for login and register

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,20 +1,25 @@
-import { Button, Heading, Layout } from 'components';
+import { Heading, Layout } from 'components';
 import { Form } from './Form';
 
+const AUTH_TYPE = 'login';
+
 export const Auth = () => {
+  const authType: string = AUTH_TYPE;
   return (
     <Layout>
       <div className="container mx-auto flex-1 flex justify-center items-center flex-col gap-10">
-        <Heading text="Welcome!" className="text-8xl" />
-        <Form />
-        <Button
-          label="SIGN UP"
-          onClick={() => {}}
-          className="mt-4 bg-softPurple text-customWhite hover:bg-darkPurple"
+        <Heading
+          text={authType === 'register' ? 'Welcome!' : 'Welcome Back!'}
+          className="text-7xl"
         />
+        <Form authType={authType} />
         <h3 className="mt-[-16px] text-customWhite tracking-widest font-rubik">
-          Already have an account?{' '}
-          <span className="font-bold cursor-pointer underline">Log in</span>
+          {authType === 'register'
+            ? 'Already have an account?'
+            : "Don't have an account?"}{' '}
+          <span className="font-bold cursor-pointer underline">
+            {authType === 'register' ? 'Log in' : 'Sign up'}
+          </span>
         </h3>
       </div>
     </Layout>
--- src/pages/auth/Form.tsx
+++ src/pages/auth/Form.tsx
@@ -1,4 +1,11 @@
-export const Form = () => {
+import * as React from 'react';
+import { Button } from 'components';
+
+interface FormProps {
+  authType: string;
+}
+
+export const Form: React.FC<FormProps> = ({ authType }) => {
   return (
     <form className="w-full flex justify-center items-center flex-col mt-8 gap-6">
       <input
@@ -12,6 +19,11 @@ export const Form = () => {
         className="w-1/4 min-w-[280px] drop-shadow-[0_2px_1.2px_rgba(0,0,0,0.3)] 
         py-3 px-4 rounded-3xl shadow font-rubik text-darkPurple focus:outline-none focus:border-darkPurple focus:border-2 placeholder-softPurple placeholder-opacity-75"
         placeholder="Password"
+      />
+      <Button
+        label={authType === 'register' ? 'SIGN UP' : 'LOGIN'}
+        onClick={() => {}}
+        className="mt-4 bg-softPurple text-customWhite hover:bg-darkPurple"
       />
     </form>
   );
